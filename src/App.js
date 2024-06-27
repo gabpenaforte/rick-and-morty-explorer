9c763dddb54a07e6e9369d6966ba1fa2705e437a
Gabriela Penaforte De Souza <gabrielapenafortedesouza@WP033599.corp.tvglobo.com.br>
2024-06-27 17:36:09 -0300

    projeto criado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { MdOutlinePersonSearch } from "react-icons/md";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="topSection">
+      <MdOutlinePersonSearch size={25} color='black'/>
+      <h1 className="text-center fw-bold ubuntu my-4">rick and morty <span className="fw-lighter">explorer</span></h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+              <Cards />
+              <Cards />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className='col-5'>
+      Cards
+    </div>
+  )
+}
+
+export default Cards;
